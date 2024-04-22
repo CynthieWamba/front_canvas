93d87ee20834baf3d9180c0a5c71822d6e324e70
ltessewamba <loica-cynthiche.tesse-wamba@etudiant.univ-rennes1.fr>
2024-04-22 19:15:03 +0100

    canva_project

---
+++ interaction.js
@@ -0,0 +1,59 @@
+
+// La création d'un Dnd requière un canvas et un interacteur.
+// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
+function DnD(canvas, interactor) {
+	// Définir ici les attributs de la 'classe'
+    this.initX = 0;
+    this.initY = 0;
+    this.finalX = 0;
+    this.finalY = 0;
+    this.isclicked = false;
+    this.interactor = interactor;
+
+	// Developper les 3 fonctions gérant les événements
+      this.clicSouris = function(evt) {
+        var pos= getMousePosition(canvas, evt)
+        this.initX = pos.x;
+        this.initY = pos.y;
+        this.isclicked = true;
+        this.interactor.onInteractionStart(this);
+      }.bind(this);
+
+      this.deplacerSouris = function(evt) {
+        if (this.isclicked) {
+          var pos = getMousePosition(canvas, evt)
+          this.finalX = pos.x;
+          this.finalY = pos.y;
+          this.isclicked = true;
+          this.interactor.onInteractionUpdate(this);
+        }
+      }.bind(this);
+
+      this.relacherSouris = function(evt) {
+        var pos= getMousePosition(canvas, evt)
+        this.finalX = pos.x;
+        this.finalY = pos.y;
+        this.isclicked = false;
+        this.interactor.onInteractionEnd(this);
+      }.bind(this);
+
+      canvas.addEventListener('mousedown', this.clicSouris, false);
+      canvas.addEventListener('mousemove', this.deplacerSouris, false);
+      canvas.addEventListener('mouseup', this.relacherSouris, false);
+
+
+  // Associer les fonctions précédentes aux évènements du canvas.
+};
+
+
+// Place le point de l'événement evt relativement à la position du canvas.
+function getMousePosition(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+};
+
+
+
